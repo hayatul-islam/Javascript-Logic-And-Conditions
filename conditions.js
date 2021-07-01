516987bc211a0e5ec5e5f36bf56f702b92d7df3f
hayatul11 <hayatulemon340@gmail.com>
2021-07-01 22:02:43 +0600

    if && else if && else condition

---
+++ conditions.js
@@ -0,0 +1,39 @@
+var a = 100
+var b = 20
+
+if(a > b){
+    console.log('A is grater than B');
+}else{
+    console.log('B is grater than A');
+}
+
+
+var n = 50;
+if(n % 2 == 0){
+    console.log("This is even number");
+}else{
+    console.log("This is odd number");
+}
+
+
+// elseif condition
+var a = 100
+var b = 100
+
+if(a > b){
+    console.log('A is grater than B');
+}else if(a < b){
+    console.log('B is grater than A');
+}else{
+    console.log('They Both are same')
+}
+
+
+var n = 0
+if(n == 0){
+    console.log('This is zero')
+}else if(n % 2 == 0){
+    console.log("This is even number")
+}else{
+    console.log("This is odd number")
+}
