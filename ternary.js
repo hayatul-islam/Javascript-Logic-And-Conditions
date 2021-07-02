656387e6a939d1068f4c82ee658a76ca61549a02
hayatul11 <hayatulemon340@gmail.com>
2021-07-02 21:05:40 +0600

    Ternary Operators

---
+++ ternary.js
@@ -0,0 +1,15 @@
+
+var n = 10
+var str = ''
+
+if(n % 2 == 0){
+    str = "Even"
+}else{
+    str = "Odd"
+}
+console.log(str)
+
+// ternary operators
+// condition ? true side : false side
+var result = n % 2 == 0 ? 'Even' : 'Odd'
+console.log(result)
